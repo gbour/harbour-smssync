03deafea45895904e7d197095841f081326a1adf
Guillaume Bour <guillaume@bour.cc>
2015-08-28 19:29:55 +0200

    [doc] add license

--- daemon/src/mqttdispatcher.cpp
+++ daemon/src/mqttdispatcher.cpp
@@ -1,3 +1,24 @@
+/*
+    Copyright (C) 2015 Guillaume Bour.
+    Contact: Guillaume Bour <buillaume@bour.cc>
+    All rights reserved.
+
+    This file is part of «harbour-smssync app».
+
+    «harbour-smssync app» is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    «harbour-smssync app» is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with «harbour-smssync app».  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "mqttdispatcher.h"
 
 #include <QDebug>
